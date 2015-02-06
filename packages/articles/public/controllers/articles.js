f511c8fd4eb7983940379ec0b320b33f4459f3b3
kouissar <kouissar@gmail.com>
2015-02-05 23:40:51 -0500

    small glitch fix

--- packages/articles/public/controllers/articles.js
+++ packages/articles/public/controllers/articles.js
@@ -99,9 +99,7 @@ angular.module('mean.articles').controller('weatherController', function($scope,
 angular.module('mean.articles').filter('timestampToDate', function() {
   return function(timestamp) {
     var date = new Date(timestamp * 1000);
-    var dateObject = date.getFullYear() + '-'
-        + ('0' + (date.getMonth() + 1)).slice(-2) + '-'
-        + ('0' + date.getDate()).slice(-2);
+    var dateObject = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
     return dateObject;
   };
 });
